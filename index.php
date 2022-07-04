2c157f1e5dfe1471143ad97cadcfb97ba815550e
Stickboy <98296968+kidrian@users.noreply.github.com>
2022-07-04 16:26:01 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
-echo 'Hello World!';
+$database = 'Driver={INTERSOLV InterBase ODBC Driver (*.gdb)};Database=C:\\MyRetail\\Windows\\Database\\MyRetail.gdb;';
+$username = 'SYSDBA';
+$password = 'masterkey';
+
+$con = odbc_connect($database, $username, $password);
+
+if ($con) {
+  echo 'Hello World!';
+} else {
+  echo 'This is not connected';
+}
 ?>
